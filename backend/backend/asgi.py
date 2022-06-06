ebef3ab282beca5dab71fbda270240a8beb160d1
Rouel <astrolabio1417@gmail.com>
2022-06-06 23:24:45 +0800

    test

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -8,9 +8,11 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 """
 
 import os
+import django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings.dev")
+django.setup()
 
 from django.core.asgi import get_asgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
 application = get_asgi_application()
--- backend/backend/settings/dev.py
+++ backend/backend/settings/dev.py
@@ -10,30 +10,30 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
-from asyncio.log import logger
 import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("BACKEND_SECRET_KEY")
+SECRET_KEY = os.environ.get("BACKEND_SECRET_KEY", "")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("BACKEND_DEBUG", default=0)
+DEBUG = os.environ.get("BACKEND_DEBUG", default=0) == "1"
 
-ALLOWED_HOSTS = os.environ.get("BACKEND_ALLOWED_HOSTS").split(" ")
+ALLOWED_HOSTS = os.environ.get("BACKEND_ALLOWED_HOSTS", "").split(" ")
 SECURE_REFERRER_POLICY = "strict-origin-when-cross-origin"
 
 # DRF
-CORS_ALLOWED_ORIGINS = os.environ.get("BACKEND_CORS_ORIGINS").split(" ")
-CSRF_TRUSTED_ORIGINS = os.environ.get("BACKEND_CSRF_TRUSTED_ORIGIN").split(" ")
-logger.warning(CSRF_TRUSTED_ORIGINS)
+CORS_ALLOWED_ORIGINS = os.environ.get("BACKEND_CORS_ORIGINS", "").split(" ")
+CSRF_TRUSTED_ORIGINS = os.environ.get("BACKEND_CSRF_TRUSTED_ORIGIN", "").split(" ")
+
+CORS_ALLOW_METHODS = ["DELETE", "GET", "OPTIONS", "PATCH", "POST", "PUT", "UPDATE"]
 
 REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
@@ -83,6 +83,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = "backend.wsgi.application"
+ASGI_APPLICATION = "backend.asgi.application"
 
 
 # Database
